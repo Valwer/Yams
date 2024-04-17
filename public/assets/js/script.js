2ff9cf5cbbf7b02a145dede7a03eac00d5058214
xR3PMz <xr3pmz@gmail.com>
2024-04-17 16:37:31 +0200

    animation un seul dés

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,19 +1,11 @@
-// // repositionnement de la navbar
-// document.addEventListener('DOMContentLoaded', function() {
-//     const navLinks = document.querySelectorAll('nav ul li');
+document.addEventListener('DOMContentLoaded', function() {
+    const rulesButton = document.getElementById('rulesButton'); // Utilisation de l'ID 'rulesButton'
+    const modal = new bootstrap.Modal(document.getElementById('exampleModal')); // Créer une instance de la modal Bootstrap
 
-//     navLinks.forEach(function(link) {
-//         link.addEventListener('click', function() {
-//             // Ajoute une classe pour activer l'animation
-//             this.classList.add('active');
-
-//             // Supprime la classe après 2 secondes
-//             setTimeout(function() {
-//                 link.classList.remove('active');
-//             }, 1000);
-//         });
-//     });
-// });
+    rulesButton.addEventListener('click', function() {
+        modal.show(); // Afficher la modal lorsque le bouton est cliqué
+    });
+});
 
 const lancerDe = function () {
     const numberDecimal = (Math.random() * 6) + 1
@@ -25,3 +17,35 @@ const lancerDe = function () {
 const result = lancerDe()
 
 console.log(lancerDe())
+
+
+        // Fonction pour simuler le lancer de dés
+        function rollDice() {
+            return Math.floor(Math.random() * 6) + 1;
+        }
+
+        // Fonction pour animer le lancer de dés
+        function animateRoll() {
+            let diceElement = document.getElementById('dice');
+            let rolls = 10; // Nombre de lancers de dés
+            let interval = 200; // Intervalle de temps entre chaque lancer en millisecondes
+
+            // Fonction récursive pour mettre à jour l'affichage des dés à chaque lancer
+            function animate() {
+                rolls--;
+                if (rolls >= 0) {
+                    setTimeout(function() {
+                        let result = rollDice();
+                        diceElement.src = `./public/assets/img/icons/dices/dices${result}.png`;
+                        animate();
+                    }, interval);
+                } else {
+                    // Lorsque tous les lancers sont terminés, afficher le résultat final
+                    let result = rollDice();
+                    diceElement.src = `./public/assets/img/icons/dices/dices${result}.png`;
+                }
+            }
+
+            // Lancer l'animation
+            animate();
+        }
