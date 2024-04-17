5d63c2953fdc7367325223239cf025d75bded7ee
Thunder Hime <takkikko@gmail.com>
2024-04-17 15:45:40 +0200

    function 5 d&s

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,38 +1,9 @@
-// // repositionnement de la navbar
-// document.addEventListener('DOMContentLoaded', function() {
-//     const navLinks = document.querySelectorAll('nav ul li');
-
-//     navLinks.forEach(function(link) {
-//         link.addEventListener('click', function() {
-//             // Ajoute une classe pour activer l'animation
-//             this.classList.add('active');
-
-//             // Supprime la classe après 2 secondes
-//             setTimeout(function() {
-//                 link.classList.remove('active');
-//             }, 1000);
-//         });
-//     });
-// });
-
-const lancerDe = function () {
-    const numberDecimal = (Math.random() * 6) + 1
-    const number = Math.trunc(numberDecimal)
-    return number
-};
-
-
-const result = lancerDe()
-
-console.log(result)
-
-function lancerDes() {
-    let result = [];
-    for (let index = 0; index < 5; index++) {
-        result.push(result);
+function lancerCinqDes() {
+    const resultats = [];
+    for (let i = 0; i < 5; i++) {
+      resultats.push(Math.floor(Math.random() * 6) + 1);
     }
-    return result;
-
-}
-let diceResult = lancerDes();
-console.log(diceResult);
+    return resultats;
+  }
+  
+  console.log(lancerCinqDes());
