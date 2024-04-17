ef40f02b0392b1083990756f026de402e2eb4914
Thunder Hime <takkikko@gmail.com>
2024-04-17 14:15:20 +0200

    lancé de des

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,16 +1,27 @@
-// repositionnement de la navbar
-document.addEventListener('DOMContentLoaded', function() {
-    const navLinks = document.querySelectorAll('nav ul li');
-    
-    navLinks.forEach(function(link) {
-        link.addEventListener('click', function() {
-            // Ajoute une classe pour activer l'animation
-            this.classList.add('active');
-            
-            // Supprime la classe après 2 secondes
-            setTimeout(function() {
-                link.classList.remove('active');
-            }, 1000);
-        });
-    });
-});
+// // repositionnement de la navbar
+// document.addEventListener('DOMContentLoaded', function() {
+//     const navLinks = document.querySelectorAll('nav ul li');
+
+//     navLinks.forEach(function(link) {
+//         link.addEventListener('click', function() {
+//             // Ajoute une classe pour activer l'animation
+//             this.classList.add('active');
+
+//             // Supprime la classe après 2 secondes
+//             setTimeout(function() {
+//                 link.classList.remove('active');
+//             }, 1000);
+//         });
+//     });
+// });
+
+const lancerDe = function () {
+    const numberDecimal = (Math.random() * 6) + 1
+    const number = Math.trunc(numberDecimal)
+    return number
+};
+
+ 
+const result = lancerDe()
+
+console.log(lancerDe())
