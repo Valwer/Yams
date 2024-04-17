bde96fc4f592c65267bbfaed25ffdf823b453be0
Thunder Hime <takkikko@gmail.com>
2024-04-17 16:09:46 +0200

    new commit

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,9 +1,22 @@
-function lancerCinqDes() {
-    const resultats = [];
-    for (let i = 0; i < 5; i++) {
-      resultats.push(Math.floor(Math.random() * 6) + 1);
+const lancerDe = function () {
+    const numberDecimal = (Math.random() * 6) + 1
+    const number = Math.trunc(numberDecimal)
+    return number
+};
+
+const roollDice = lancerDe()
+
+function rollDices() {
+    let result = [];
+    for (let index = 0; index < 5; index++) {
+        result.push(roollDice);
+        if ((roollDice * 4) === (roollDice * 4)) {
+            console.log('carré')
+            break
+        } else if ((roollDice * 5) === (roollDice * 5)) {
+            console.log('yams')
+        }
     }
-    return resultats;
-  }
-  
-  console.log(lancerCinqDes());
+    return result;
+}
+console.log(rollDices());
