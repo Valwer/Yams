8eb99d4104ac67d855d87d32f57948397c606ffe
Thunder Hime <takkikko@gmail.com>
2024-04-18 09:39:18 +0200

    delete one function and modify an another

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,36 +1,23 @@
 // Fonction pour simuler le lancer d'un dé
-function lancerDe() {
+function getRandomValue() {
 
     return Math.floor(Math.random() * 6) + 1;
 }
 
-// Fonction pour simuler un lancer de tous les dés
-function lancerDes() {
-    let resultats = [];
-
-    // Effectuer 5 lancers de dés et stocker les résultats dans le tableau
-    for (let i = 0; i < 5; i++) {
-        resultats.push(lancerDe());
-    }
-
-    // Retourner le tableau contenant les résultats des lancers
-    return resultats;
-}
-
 
 // Fonction ReRollTheDice : Réinitialise les valeurs des dés avec de nouvelles valeurs aléatoires
-function ReRollTheDice(dice) {
+function rollTheDices(dice = [0, 0, 0, 0, 0]) {
     // Créer un nouveau tableau pour stocker les nouvelles valeurs des dés
     let newDice = [];
 
     // Parcourir le tableau de dés initial
     for (let i = 0; i < dice.length; i++) {
-        let newDiceValue = Math.floor(Math.random() * 6) + 1;
-        newDice.push(newDiceValue);
+        newDice.push(getRandomValue());
     }
 
     return newDice;
 }
+// console.log(rollTheDices([1,3,5]));
 
 function displayDice(dice) {
     // Sélectionner l'élément HTML qui contient les dés
@@ -51,20 +38,12 @@ function displayDice(dice) {
 }
 
 // Exemple d'utilisation de la fonction displayDice
-const dice = lancerDes(); // Exemple de tableau de dés
+const dice = rollTheDices(); // Exemple de tableau de dés
 displayDice(dice); // Appel de la fonction pour afficher les dés sur la page
 
-let resultatDes = lancerDes();
+let resultatDes = rollTheDices();
 console.log("Résultats des lancers des dés :", resultatDes);
 
 
-let newDice = ReRollTheDice(dice); // Appel de la fonction ReRollTheDice avec le tableau initial
+let newDice = rollTheDices(dice); // Appel de la fonction ReRollTheDice avec le tableau initial
 console.log("Nouveau tableau de dés :", newDice);
-
-
-
-
-
-
-
-
