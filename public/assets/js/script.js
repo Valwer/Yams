2c97517f5ac31af0ea132ec1e579d2f5ef1e8b9d
Valentin <cranenoir@gmail.com>
2024-04-18 14:38:42 +0200

    Lancer et relancer les dès

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,49 +1,65 @@
+let dices = [];
+let keptDices = [];
+
 // Fonction pour simuler le lancer d'un dé
 function getRandomValue() {
-
     return Math.floor(Math.random() * 6) + 1;
 }
 
-
-// Fonction ReRollTheDice : Réinitialise les valeurs des dés avec de nouvelles valeurs aléatoires
-function rollTheDices(dice = [0, 0, 0, 0, 0]) {
-    // Créer un nouveau tableau pour stocker les nouvelles valeurs des dés
+// Fonction pour réinitialiser les valeurs des dés avec de nouvelles valeurs aléatoires
+function rollTheDices(dice = [0, 0, 0, 0, 0], selectedIndexes) {
     let newDice = [];
-
-    // Parcourir le tableau de dés initial
     for (let i = 0; i < dice.length; i++) {
         newDice.push(getRandomValue());
     }
-
     return newDice;
 }
-// console.log(rollTheDices([1,3,5]));
 
-function displayDice(dice) {
-    // Sélectionner l'élément HTML qui contient les dés
+// Fonction pour mettre à jour l'affichage des dés
+function updateDiceDisplay(dice) {
     const diceContainer = document.querySelector('.gameZone');
-
-    // Nettoyer le contenu précédent
     diceContainer.innerHTML = '';
-
-    // Parcourir le tableau de dés
     dice.forEach((value, index) => {
-        // Créer un élément pour représenter le dé
         const diceElement = document.createElement('div');
         diceElement.classList.add('face');
-        diceElement.classList.add('face' + value); // Ajouter une classe pour représenter la face du dé
-        diceElement.textContent = value; // Afficher la valeur du dé
-        diceContainer.appendChild(diceElement); // Ajouter le dé au conteneur des dés
+        diceElement.classList.add('face' + value);
+        diceElement.classList.remove('d-none');
+        diceElement.textContent = value;
+        diceElement.addEventListener('click', function() {
+            // Ajouter ou retirer la classe .selected-dice lorsqu'on clique sur un dé
+            diceElement.classList.toggle('selected-dice');
+        });
+        diceContainer.appendChild(diceElement);
     });
 }
 
-// Exemple d'utilisation de la fonction displayDice
-const dice = rollTheDices(); // Exemple de tableau de dés
-displayDice(dice); // Appel de la fonction pour afficher les dés sur la page
-
-let resultatDes = rollTheDices();
-console.log("Résultats des lancers des dés :", resultatDes);
 
 
-let newDice = rollTheDices(dice); // Appel de la fonction rollTheDices avec le tableau initial
-console.log("Nouveau tableau de dés :", newDice);
+// Gestionnaire d'événement pour le bouton de relance
+const diceIcon = document.getElementById('dice-icon');
+diceIcon.addEventListener('click', function() {
+    // Obtenir les dés sélectionnés
+    const selectedDiceElements = document.querySelectorAll('.selected-dice');
+    const selectedDiceIndexes = Array.from(selectedDiceElements).map(element => parseInt(element.textContent));
+    console.log(`Dès initiaux : ${keptDices}`);
+    // Supprimer les dés sélectionnés de keptDices
+    selectedDiceIndexes.forEach(el => {
+        foundedIndex = keptDices.indexOf(el);
+        keptDices.splice(foundedIndex,1);
+    });
+    console.log(`Dès après suppression : ${keptDices}`);
+    console.log(selectedDiceIndexes);
+    // Vérifier si aucun dé n'est sélectionné
+    if (selectedDiceIndexes.length == 0 && keptDices.length < 5) {
+        newDices = rollTheDices();
+        console.log(newDices);
+        keptDices = keptDices.concat(newDices);
+    } else if (selectedDiceIndexes.length > 0) {
+        newDices = rollTheDices(selectedDiceIndexes);
+        console.log(`Nouveau lancé : ${newDices}`);
+        keptDices = keptDices.concat(newDices);
+    }
+    console.log(keptDices);
+    updateDiceDisplay(keptDices);
+});
+
