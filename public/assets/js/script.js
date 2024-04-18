a3a8f6f2798279754ee52c8bd237b05ebe747dd8
Thunder Hime <takkikko@gmail.com>
2024-04-18 12:13:03 +0200

    commit

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -4,8 +4,7 @@ function getRandomValue() {
     return Math.floor(Math.random() * 6) + 1;
 }
 
-
-// Fonction ReRollTheDice : Réinitialise les valeurs des dés avec de nouvelles valeurs aléatoires
+// Fonction rollTheDices : Réinitialise les valeurs des dés avec de nouvelles valeurs aléatoires
 function rollTheDices(dice = [0, 0, 0, 0, 0]) {
     // Créer un nouveau tableau pour stocker les nouvelles valeurs des dés
     let newDice = [];
@@ -21,7 +20,7 @@ function rollTheDices(dice = [0, 0, 0, 0, 0]) {
 
 function displayDice(dice) {
     // Sélectionner l'élément HTML qui contient les dés
-    const diceContainer = document.querySelector('.gameZone');
+    const diceContainer = document.querySelector('#dice');
 
     // Nettoyer le contenu précédent
     diceContainer.innerHTML = '';
@@ -47,3 +46,25 @@ console.log("Résultats des lancers des dés :", resultatDes);
 
 let newDice = rollTheDices(dice); // Appel de la fonction rollTheDices avec le tableau initial
 console.log("Nouveau tableau de dés :", newDice);
+
+// Opérations pour les points
+let diceValue;
+// Fonction pour calculer les totaux pour chaque valeur de dé
+function calculateTotalForDiceValue(diceResults, diceValue) {
+    return diceResults.filter(value => value === diceValue).length * diceValue;
+}
+
+// Simuler le lancer des dés, afficher les résultats, et calculer les totaux
+let diceResults = rollTheDices();
+displayDice(diceResults);
+console.log("Résultats des lancers des dés :", diceResults);
+
+// Afficher les totaux pour chaque valeur de dé de 1 à 6
+Array.from({ length: 6 }, (_, i) => i + 1).forEach(value => {
+    console.log(`Total pour les dés de valeur ${value}:`, calculateTotalForDiceValue(diceResults, value));
+});
+
+if (diceResults.includes((diceValue === diceValue) * 3)) {
+    console.log('Brelan')
+
+}
