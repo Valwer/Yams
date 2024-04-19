e16ffd3e85c2d4db8dc17692013b26f5cc0c8d7e
Thunder Hime <takkikko@gmail.com>
2024-04-19 08:48:22 +0200

    new commit

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,20 +1,3 @@
-// // repositionnement de la navbar
-// document.addEventListener('DOMContentLoaded', function() {
-//     const navLinks = document.querySelectorAll('nav ul li');
-
-//     navLinks.forEach(function(link) {
-//         link.addEventListener('click', function() {
-//             // Ajoute une classe pour activer l'animation
-//             this.classList.add('active');
-
-//             // Supprime la classe après 2 secondes
-//             setTimeout(function() {
-//                 link.classList.remove('active');
-//             }, 1000);
-//         });
-//     });
-// });
-
 const lancerDe = function () {
     const numberDecimal = (Math.random() * 6) + 1
     const number = Math.trunc(numberDecimal)
