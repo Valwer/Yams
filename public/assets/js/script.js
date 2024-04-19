fe27a6a239dc028d822b08e8f680835fa879065d
Thunder Hime <takkikko@gmail.com>
2024-04-19 10:13:40 +0200

    fdbvcn xfb v

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,18 +1,3 @@
-const lancerDe = function () {
-    const numberDecimal = (Math.random() * 6) + 1
-    const number = Math.trunc(numberDecimal)
-    return number
-};
-
-
-const result = lancerDe()
-
-console.log(result)
-
-function lancerDes() {
-    let result = [];
-    for (let index = 0; index < 5; index++) {
-        result.push(result);
 let dices = [];
 let keptDices = [];
 rollsLeft = 3;
