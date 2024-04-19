b025255a3d0c0cb5227bf9bc3efba3e9f0ba2581
Thunder Hime <takkikko@gmail.com>
2024-04-19 11:13:23 +0200

    vxcgfdv

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -28,10 +28,7 @@ const lancerDe = function () {
     return number
 };
 
-
 const result = lancerDe()
-
-console.log(lancerDe())
 
 
 // Fonction pour simuler le lancer de dés
@@ -66,7 +63,6 @@ function animateRoll() {
 }
 
 
-
 // Fonction pour simuler le lancer d'un dé
 function getRandomValue() {
     return Math.floor(Math.random() * 6) + 1;
@@ -92,75 +88,56 @@ function updateDiceDisplay(dice) {
         diceContainer.innerHTML += diceElement;
     });
 
-  selectedDices = document.querySelectorAll(".dice");
+    selectedDices = document.querySelectorAll(".dice");
 
-  selectedDices.forEach((selectedDice) => {
-    selectedDice.addEventListener("click", function () {
-      // Ajouter ou retirer la classe .selected-dice lorsqu'on clique sur un dé
-      selectedDice.classList.toggle("selected-dice");
+    selectedDices.forEach((selectedDice) => {
+        selectedDice.addEventListener("click", function () {
+            // Ajouter ou retirer la classe .selected-dice lorsqu'on clique sur un dé
+            selectedDice.classList.toggle("selected-dice");
+        });
     });
-  });
 }
 
 // Gestionnaire d'événement pour le bouton de relance
 const diceIcon = document.getElementById("dice-icon");
 diceIcon.addEventListener("click", function () {
-  if (rollsLeft > 0) {
-    // Vérifie le nombre de lancers restants
-    // Obtenir les dés sélectionnés
-    const selectedDiceElements = document.querySelectorAll(".selected-dice");
-    const selectedDiceIndexes = Array.from(selectedDiceElements).map(
-      (element) => parseInt(element.textContent)
-    );
-    console.log(`Dès initiaux : ${keptDices}`);
-    // Supprimer les dés sélectionnés de keptDices
-    selectedDiceIndexes.forEach((el) => {
-      foundedIndex = keptDices.indexOf(el);
-      keptDices.splice(foundedIndex, 1);
-    });
-    console.log(`Dès après suppression : ${keptDices}`);
-    console.log(selectedDiceIndexes);
-    // Vérifier si aucun dé n'est sélectionné
-    if (selectedDiceIndexes.length == 0 && keptDices.length < 5) {
-      newDices = rollTheDices();
-      console.log(newDices);
-      keptDices = keptDices.concat(newDices);
-    } else if (selectedDiceIndexes.length > 0) {
-      newDices = rollTheDices(selectedDiceIndexes);
-      console.log(`Nouveau lancé : ${newDices}`);
-      keptDices = keptDices.concat(newDices);
+    if (rollsLeft > 0) {
+        // Vérifie le nombre de lancers restants
+        // Obtenir les dés sélectionnés
+        const selectedDiceElements = document.querySelectorAll(".selected-dice");
+        const selectedDiceIndexes = Array.from(selectedDiceElements).map(
+            (element) => parseInt(element.textContent)
+        );
+        console.log(`Dès initiaux : ${keptDices}`);
+        // Supprimer les dés sélectionnés de keptDices
+        selectedDiceIndexes.forEach((el) => {
+            foundedIndex = keptDices.indexOf(el);
+            keptDices.splice(foundedIndex, 1);
+        });
+        console.log(`Dès après suppression : ${keptDices}`);
+        console.log(selectedDiceIndexes);
+        // Vérifier si aucun dé n'est sélectionné
+        if (selectedDiceIndexes.length == 0 && keptDices.length < 5) {
+            newDices = rollTheDices();
+            console.log(newDices);
+            keptDices = keptDices.concat(newDices);
+        } else if (selectedDiceIndexes.length > 0) {
+            newDices = rollTheDices(selectedDiceIndexes);
+            console.log(`Nouveau lancé : ${newDices}`);
+            keptDices = keptDices.concat(newDices);
+        }
+        rollsLeft--; // Réduire le nombre de lancers restants
+        console.log(`Lancers restants : ${rollsLeft}`);
+        console.log(keptDices);
+        updateDiceDisplay(keptDices);
+    } else {
+        console.log("Vous avez atteint le nombre maximal de lancers pour ce tour.");
     }
-    rollsLeft--; // Réduire le nombre de lancers restants
-    console.log(`Lancers restants : ${rollsLeft}`);
-    console.log(keptDices);
-    updateDiceDisplay(keptDices);
-} else {
-    console.log("Vous avez atteint le nombre maximal de lancers pour ce tour.");
-}
 });
 
 let newDice = rollTheDices(dices); // Appel de la fonction rollTheDices avec le tableau initial
 console.log("Nouveau tableau de dés :", newDice);
 
-// afficher le cumul des 1
-
-let total = 0;
-keptDices.forEach((dice) => {
-    if (dice === 1) {
-        total += dice;
-    }
-});
-console.log("Total des dés 1 : " + total);
-
-// afficher le cumul des 4
-
-total = 0;
-keptDices.forEach((dice) => {
-    if (dice === 4) {
-        total += 1;
-    }
-});
-console.log("Total des dés 4 : " + total);
 
 // afficher le cumul des 5
 
@@ -183,50 +160,41 @@ keptDices.forEach((dice) => {
 });
 console.log("total des dés 6 :" + total);
 
-// afficher le cumul des 3
-
-total = 0;
-keptDices.forEach((dice) => {
-    if (dice === 3) {
-        total += 1;
-    }
-});
-console.log("Total des dés 3 : " + total);
 
 // afficher le cumul des 2
 
 function cumul(array, diceValue) {
-  total = 0;
-  array.forEach((dice) => {
-    if (dice === diceValue) {
-      total += dice;
-    }
-  });
-  console.log("Total des dés : " + total);
+    total = 0;
+    array.forEach((dice) => {
+        if (dice === diceValue) {
+            total += dice;
+        }
+    });
+    console.log("Total des dés : " + total);
 }
 cumul(keptDices, 4);
 
 function calculate(diceRolls) {
-  const diceCounts = {};
+    const diceCounts = {};
 
-  // Compter les occurrences de chaque valeur de dé
-  diceRolls.forEach((diceValue) => {
-    if (diceCounts[diceValue]) {
-      diceCounts[diceValue]++;
-    } else {
-      diceCounts[diceValue] = 1;
+    // Compter les occurrences de chaque valeur de dé
+    diceRolls.forEach((diceValue) => {
+        if (diceCounts[diceValue]) {
+            diceCounts[diceValue]++;
+        } else {
+            diceCounts[diceValue] = 1;
+        }
+    });
+
+    // Vérifier s'il y a un brelan
+    for (let value in diceCounts) {
+        if (diceCounts[value] === 3) {
+            console.log("Brelan");
+            return;
+        }
     }
-  });
 
-  // Vérifier s'il y a un brelan
-  for (let value in diceCounts) {
-    if (diceCounts[value] === 3) {
-      console.log("Brelan");
-      return;
-    }
-  }
-
-  console.log("Pas de brelan");
+    console.log("Pas de brelan");
 }
 
 // Exemple d'utilisation
