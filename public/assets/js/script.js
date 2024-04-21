4af523769197687d5e2b565896f9113890fdc268
Thunder Hime <takkikko@gmail.com>
2024-04-22 00:46:30 +0200

    ajout animation, tableau score, affichage des totaux ...

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,92 +1,130 @@
 // Déclaration de variables-------------
 let dices = [];
 let keptDices = [];
+let selectedCategory = false;
+let nextRound = true;
 rollsLeft = 3;
 //--------------------------------------
 
 // Les sélecteurs
+const diceTableTop = document.getElementById("dice-table-top");
 const diceIcon = document.getElementById("dice-icon");
-const scoreCells = document.querySelectorAll(".space-col");
+const diceInfo = document.querySelector(".dice-info");
+const scoreCells = document.querySelectorAll(".score-cell");
 
-// Toutes les Fonctions-------------
+// --------------------------------------
+//              Fonctions
+// --------------------------------------
 
-const lancerDe = function () {
-    const numberDecimal = (Math.random() * 6) + 1
-    const number = Math.trunc(numberDecimal)
-    return number
-};
-
-// Fonction pour simuler le lancer de dés
-function rollDice() {
-    return Math.floor(Math.random() * 6) + 1;
-}
-
-// Fonction pour animer le lancer de dés
-function animateRoll() {
-    let diceElement = document.getElementById('dice');
-    let rolls = 10; // Nombre de lancers de dés
-    let interval = 200; // Intervalle de temps entre chaque lancer en millisecondes
-
-    // Fonction récursive pour mettre à jour l'affichage des dés à chaque lancer
-    function animate() {
-        rolls--;
-        if (rolls >= 0) {
-            setTimeout(function () {
-                let result = rollDice();
-                diceElement.src = `./public/assets/img/icons/dices/dic${result}.png`;
-                animate();
-            }, interval);
-        } else {
-            // Lorsque tous les lancers sont terminés, afficher le résultat final
-            let result = rollDice();
-            diceElement.src = `./public/assets/img/icons/dices/dic${result}.png`;
-        }
-    }
-
-    // Lancer l'animation
-    animate();
-}
-
-// Fonction pour simuler le lancer d'un dé
+/**
+ * Récupérer une valeur aléatoire
+ * @returns une valeur aléatoire
+ */
 function getRandomValue() {
     return Math.floor(Math.random() * 6) + 1;
 }
 
-// Fonction pour réinitialiser les valeurs des dés avec de nouvelles valeurs aléatoires
-function rollTheDices(dice = [0, 0, 0, 0, 0], selectedIndexes) {
+/**
+ * Fonction pour réinitialiser les valeurs des dés avec de nouvelles valeurs aléatoires
+ * @param {number[]} dices 
+ * @returns un tableau avec des valeurs aléatoires
+ */
+function rollTheDices(dices = [0, 0, 0, 0, 0]) {
     let newDice = [];
-    for (let i = 0; i < dice.length; i++) {
+    for (let i = 0; i < dices.length; i++) {
         newDice.push(getRandomValue());
     }
     return newDice;
 }
 
-function updateDiceDisplay(dice) {
-    // Sélectionner l'élément HTML qui contient les dés
-    const diceContainer = document.querySelector("#dice");
-
-    // Nettoyer le contenu précédent
-    diceContainer.innerHTML = "";
-    dice.forEach((value, index) => {
-        let diceElement = `<img class="dice des${index}" src="./public/assets/img/icons/dices/dic${value}.png" alt="Dé" data-value="${value}" />`;
-        diceContainer.innerHTML += diceElement;
-    });
-
-    selectedDices = document.querySelectorAll(".dice");
-
-    selectedDices.forEach((selectedDice) => {
-        selectedDice.addEventListener("click", function () {
-            // Ajouter ou retirer la classe .selected-dice lorsqu'on clique sur un dé
-            selectedDice.classList.toggle("selected-dice");
-        });
+/**
+ * Fonction pour mettre à jour l'affichage des Dés avec les nouvelles valeurs fourni en paramètre
+ * 
+ * @param {number[]} dices 
+ */
+function updateDicesDisplay(dices) {
+    dices.forEach((value, index) => {
+        let dice = diceTableTop.querySelector(`.des${index}`);
+        if (dice == null) {
+            dice = createDiceElement(index);
+        }
+        dice.alt = `Dé ${value}`;
+        dice.dataset.value = value;
+        dice.classList.remove("selected-dice");
+        animate(dice, value);
     });
 }
 
+/**
+ * Fonction pour créer un élément HTML représentant un dé.
+ * Le dé est ajouté au Tabletop.
+ * Chaque dé a un listener sur le click pour selectionner/déselectionner
+ * @param {number} index 
+ * @returns Un element HTML
+ */
+function createDiceElement(index) {
+    let dice = document.createElement("img");
+    dice.classList.add("dice", `des${index}`);
+    dice.addEventListener("click", function (event) {
+        // Ajouter ou retirer la classe .selected-dice lorsqu'on clique sur un dé
+        event.target.classList.toggle("selected-dice");
+    });
+    diceTableTop.appendChild(dice);
+    return dice;
+}
+
+/**
+ * Fonction pour animer les faces des dés
+ * @param {HTMLElement} diceElement 
+ * @param {number} finalDiceValue 
+ */
+async function animate(diceElement, finalDiceValue) {
+    const numberOfRolls = 10;
+    let interval = 200;
+    // image initial (pour eviter le lien mort)
+    diceElement.src = `./public/assets/img/icons/dices/dic${getRandomValue()}.png`;
+    for (let i = numberOfRolls; i >= 0; i--) {
+        if (i > 0) {
+            // Afficher une image basée sur une valeur aléatoire pour simuler le roulement du dé
+            // Sleep à chaque fois pour laisser le temps d'afficher chaque "pseudo" roulement
+            await sleep(interval);
+            let randomDisplayValue = getRandomValue();
+            diceElement.src = `./public/assets/img/icons/dices/dic${randomDisplayValue}.png`;
+        } else {
+            // Affiche la vraie valeur qui a été retenu (fin du roulement aléatoire)
+            diceElement.src = `./public/assets/img/icons/dices/dic${finalDiceValue}.png`;
+        }
+    }
+}
+
+/**
+ * Simule un temps d'attente en ms
+ * @param {number} ms 
+ * @returns une promesse
+ */
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+/**
+ * Lancer de dés
+ */
 function lancerDes() {
+    const selectedDiceElements = document.querySelectorAll(".selected-dice");
+    if (selectedDiceElements.length == 0 && !nextRound) {
+        return;
+    }
+
+    if(nextRound) {
+        nextRound = false;
+        selectedCategory = false;
+        rollsLeft = 3;
+        keptDices = [];
+    }
+
     if (rollsLeft > 0) {
         // Vérifie le nombre de lancers restants
         // Obtenir les dés sélectionnés
-        const selectedDiceElements = document.querySelectorAll(".selected-dice");
         const selectedDiceIndexes = Array.from(selectedDiceElements).map(
             (element) => parseInt(element.dataset.value)
         );
@@ -99,15 +137,15 @@ function lancerDes() {
         console.log(`Dès après suppression : ${keptDices}`);
         console.log(selectedDiceIndexes);
         // Vérifier si aucun dé n'est sélectionné
-        if (selectedDiceIndexes.length == 0 && keptDices.length < 5) {
+        if (selectedDiceIndexes.length == 0) {
             newDices = rollTheDices();
-            console.log(newDices);
-            keptDices = keptDices.concat(newDices);
         } else if (selectedDiceIndexes.length > 0) {
             newDices = rollTheDices(selectedDiceIndexes);
-            console.log(`Nouveau lancé : ${newDices}`);
-            keptDices = keptDices.concat(newDices);
         }
+
+        console.log(`Nouveau lancé : ${newDices}`);
+        keptDices = keptDices.concat(newDices);
+
         // Appeler calculatePoint() pour chaque combinaison possible et afficher les résultats
         const combinations = ["brelan", "carre", "full", "petite_suite", "grande_suite", "yams", "chance", "cumul_1", "cumul_2", "cumul_3", "cumul_4", "cumul_5", "cumul_6"];
         combinations.forEach(combination => {
@@ -115,24 +153,35 @@ function lancerDes() {
             console.log(`Points pour ${combination} : ${points}`);
         });
 
-        
+
         rollsLeft--; // Réduire le nombre de lancers restants
         console.log(`Lancers restants : ${rollsLeft}`);
         console.log(keptDices);
-        updateDiceDisplay(keptDices);
+        diceInfo.innerHTML = rollsLeft;
+        updateDicesDisplay(keptDices);
     } else {
         console.log("Vous avez atteint le nombre maximal de lancers pour ce tour.");
     }
 }
 
-// Fonction pour calculer le score pour une catégorie donnée
+/**
+ * Calculer le score pour une catégorie donnée
+ * @param {string} category 
+ * @param {number[]} dices 
+ * @returns 
+ */
 function calculateScoreForCategory(category, dices) {
     // Utilisez la fonction calculatePoint avec la catégorie et les dés pour obtenir le score
     return calculatePoint(category, dices);
 }
 
-// Calculer les points en fonction de l'opération et des dés rentrés en paramètre
-
+/**
+ * Calculer les points en fonction de l'opération et des dés rentrés en paramètre
+ * 
+ * @param {string} operation 
+ * @param {number[]} dices 
+ * @returns Un score relatif à l'opération choisie
+ */
 function calculatePoint(operation, dices) {
     let total = 0;
     let brelan = false, carre = false, full = false, smallStraight = false, largeStraight = false, yams = false;
@@ -149,7 +198,6 @@ function calculatePoint(operation, dices) {
                 }
             }
             if (brelan) {
-                categoriesScores.Brelan.push(scoreToAdd);
                 total = scoreToAdd;
             }
             break;
@@ -163,7 +211,6 @@ function calculatePoint(operation, dices) {
                 }
             }
             if (carre) {
-                categoriesScores.Carré.push(scoreToAdd);
                 total = scoreToAdd;
             }
             break;
@@ -175,7 +222,6 @@ function calculatePoint(operation, dices) {
                 scoreToAdd = 25;
             }
             if (full) {
-                categoriesScores.Full.push(scoreToAdd);
                 total = scoreToAdd;
             }
             break;
@@ -185,7 +231,6 @@ function calculatePoint(operation, dices) {
                 scoreToAdd = 30;
             }
             if (smallStraight) {
-                categoriesScores.PetiteSuite.push(scoreToAdd);
                 total = scoreToAdd;
             }
             break;
@@ -195,7 +240,6 @@ function calculatePoint(operation, dices) {
                 scoreToAdd = 40;
             }
             if (largeStraight) {
-                categoriesScores.GrandeSuite.push(scoreToAdd);
                 total = scoreToAdd;
             }
             break;
@@ -205,95 +249,161 @@ function calculatePoint(operation, dices) {
                 scoreToAdd = 50;
             }
             if (yams) {
-                categoriesScores.Yams.push(scoreToAdd);
                 total = scoreToAdd;
             }
             break;
         case "chance":
             scoreToAdd = dices.reduce((acc, curr) => acc + curr, 0);
-            categoriesScores.Chance.push(scoreToAdd);
             total = scoreToAdd;
             break;
         default:
             console.log("opération non prise en charge");
-            return;
+            return 0;
     }
     console.log(`Score pour ${operation}: ${total}`);
-    console.log(`Total général: ${categoriesScores.getTotal()}`);
+    console.log(`Total général: ${categoriesScores.getGlobalScore()}`);
     return total;
 }
 
-
-
-//--------------------------------------
-// Script principal
-
-const result = lancerDe()
-console.log(lancerDe())
-let newDice = rollTheDices(dices); // Appel de la fonction rollTheDices avec le tableau initial
-console.log("Nouveau tableau de dés :", newDice);
-
-
-// Ecouteurs
-diceIcon.addEventListener("click", lancerDes); // Gestionnaire d'événement pour le bouton de relance
-
-// Objet yamsScores contenant des tableaux
-
-let yamsScores = {
-    As: null,
-    Deux: null,
-    Trois: null,
-    Quatre: null,
-    Cinq: null,
-    Six: null,
-    Total: null,
-    Bonus: null,
-    Total1: null,
-    Brelan: null,
-    Carré: null,
-    Full: null,
-    PetiteSuite: null,
-    GrandeSuite: null,
-    Yams: null,
-    Chance: null,
-    Total2: null,
-    Score: null,
-
-    getTotal: function () {
-        let total = 0;
-        for (let key in this) {
-            if (Array.isArray(this[key])) {
-                total += this[key].reduce((acc, val) => acc + val, 0);
-            }
-        }
-        return total;
-    }
-};
-
+/**
+ * Ajouter le score dans l'objet des scores
+ * @param {string} categorie 
+ * @param {number} score 
+ */
 function ajouterScore(categorie, score) {
-    if (yamsScores.hasOwnProperty(categorie)) {
-        yamsScores[categorie].push(score);
+    if (categoriesScores.hasOwnProperty(categorie)) {
+        categoriesScores[categorie] = score;
     } else {
         console.error("Catégorie non valide");
     }
 }
+function refreshGame() {
+    refreshTableScores();
+    //TODO: Ici rajouter l'appel à la méthode IsAllCompleted de l'objet
+    // Et conditionner l'affichage ou non du score 
+}
 
-// Ajouter des scores aux catégories
-ajouterScore("Brelan", 25);
+function refreshTableScores() {
+    const uncategorizedTotal = document.querySelector("#uncategorized-total");
+    const bonus = document.querySelector("#uncategorized-bonus");
+    const uncategorizedTotalWithBonus = document.querySelector("#uncategorized-full");
+    const categorizedTotal = document.querySelector("#categorized-total");
+    const globalScore = document.querySelector("#global-score");
 
-// Afficher le score total
-console.log("Score Total:", yamsScores.getTotal());
+    uncategorizedTotal.innerHTML = categoriesScores.getUncategorizedTotal();
+    bonus.innerHTML = categoriesScores.getBonus();
+    uncategorizedTotalWithBonus.innerHTML = categoriesScores.getUncategorizedTotalWithBonus();
+    categorizedTotal.innerHTML = categoriesScores.getCategorizedTotal();
+    globalScore.innerHTML = categoriesScores.getGlobalScore();
+}
+
+// Déclaration Objet conteneur des scores
+let categoriesScores = {
+    as: null,
+    deux: null,
+    trois: null,
+    quatre: null,
+    cinq: null,
+    six: null,
+    brelan: null,
+    carre: null,
+    full: null,
+    petiteSuite: null,
+    grandeSuite: null,
+    yams: null,
+    chance: null,
+
+    getBonus: function () {
+        console.log(this);
+        const uncategorizedTotal = this.getUncategorizedTotal();
+        if (uncategorizedTotal > 63) {
+            return 35;
+        }
+
+        return 0;
+    },
+    getUncategorizedTotal: function () {
+        return this.as + this.deux + this.trois + this.quatre + this.cinq + this.six;
+    },
+    getUncategorizedTotalWithBonus: function () {
+        return this.getUncategorizedTotal() + this.getBonus();
+    },
+    getCategorizedTotal: function () {
+       return this.brelan + this.carre + this.full + this.petiteSuite + this.grandeSuite + this.yams + this.chance;
+    },
+    getGlobalScore: function () {
+        return this.getUncategorizedTotalWithBonus() + this.getCategorizedTotal();
+    },
+    isAllCompleted: function () {
+        // Verifier si toutes propriétés sont différentes de null alors on a fini (true) sinon non (false)
+        return false;
+    }
+};
+
+//--------------------------------------
+//          Script principal
+//--------------------------------------
+diceInfo.innerHTML = rollsLeft;
+let newDices = rollTheDices(dices); // Appel de la fonction rollTheDices avec le tableau initial
+console.log("Nouveau tableau de dés :", newDices);
+
+// Ecouteurs
+diceIcon.addEventListener("click", lancerDes); // Gestionnaire d'événement pour le bouton de relance
 
 // Parcourir chaque cellule et ajoutez un événement de clic à chacune
 scoreCells.forEach((cell) => {
-    cell.addEventListener("click", function() {
+    cell.addEventListener("mouseenter", function () {
+        if (cell.classList.contains("filled") || selectedCategory || nextRound) {
+            return;
+        }
+
         // Récupérez la catégorie de la cellule
-        const category = cell.querySelector("td:first-child").textContent.trim();
+        const category = cell.dataset.category;
         // Calculez le score pour cette catégorie
         const score = calculateScoreForCategory(category, keptDices); // Utilisez keptDices comme dés pour le calcul
-        // Mettez à jour le contenu de la cellule avec le score calculé
-        cell.querySelector("td:last-child").textContent = score;
-        // Ajoutez la classe "filled" pour indiquer que la cellule est remplie avec un score
-        cell.classList.add("filled");
+
+        // Mettez à jour le contenu de la cellule avec le score calculé (uniquement affichage ici)
+        cell.innerHTML = score;
+        cell.dataset.value = score;
     });
+    cell.addEventListener("mouseout", function () {
+
+        // Si l'utilisateur a choisi cette case (click), on evite de supprimer son contenu
+        if (cell.classList.contains("filled")) {
+            return;
+        }
+
+        cell.innerHTML = "";
+        delete cell.dataset.value;
+    });
+    cell.addEventListener("click", (event) => {
+        // Si la case est déjà rempli précédemment ou que la catégorie a déjà été selectionné sur cette manche
+        // On ne continue pas
+        if (cell.classList.contains("filled") || selectedCategory) {
+            return;
+        }
+
+        // Indicateur permet d'identifier si l'utilisateur a déjà selectionné une catégorie sur ce tour
+        selectedCategory = true;
+        const category = cell.dataset.category;
+        // Ajout du score dans l'objet des scores
+        categoriesScores[category] = Number(cell.dataset.value);
+        // Ajout de la classe pour eviter l'affichage/calcul superflu
+        cell.classList.add("filled");
+
+        // On indique que la prochaine manche sera une nouvelle
+        // Afin de pouvoir réinitialiser les tentatives
+        nextRound = true;
+        refreshGame();
+    })
 });
+
+
+// TODO: Rajouter les "uncategorized" (1 à 6)
+// TODO: Regarder pourquoi les Petites et Grandes suites ne semblent pas fonctionner
+// TODO: Identifier quand le jeu est terminé et afficher le score du joueur en conséquence
+// TODO: Revoir problème sur les règles (la partie écrite)
+// TODO: Tester toutes les combinaisons
+// TODO: (Fait ?) Rajouter le controle de séléction des dés pour éviter d'incrémenter le compteur de lancement de dés lorsqu'il n'y a aucune selection du joueur
+// TODO: Revoir l'animation, le refresh reactulise l'affichage tout les dés même les non selectionnés
+
